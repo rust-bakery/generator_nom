9245d3233d14e203b06bc24593185cce5da0243a
Geoffroy Couprie <geo.couprie@gmail.com>
2017-08-30 14:57:26 +0200

    replace BufReader with circular

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,21 @@
 #[macro_use]
 extern crate nom;
 extern crate flavors;
+extern crate circular;
 
 use std::env;
 use std::cmp::min;
 use std::fs::File;
-use std::io::{BufRead,BufReader};
+use std::io::{Read,Write};
 use std::ops::{Generator, GeneratorState};
 
 use flavors::parser::{header,complete_tag,tag_header,TagType};
 use nom::{HexDisplay,IResult,Offset};
+use circular::Buffer;
 
 mod types;
 
 use types::*;
-
-#[derive(Clone,Copy,Debug,PartialEq,Eq)]
-pub struct MyTagHeader {
-  pub tag_type:  TagType,
-  pub data_size: u32,
-  pub timestamp: u32,
-  pub stream_id: u32,
-}
 
 fn main() {
   let mut args = env::args();
@@ -37,25 +31,29 @@ fn main() {
 
 fn run(filename: &str) -> std::io::Result<()> {
   let mut file = File::open(filename)?;
-  let mut reader = BufReader::with_capacity(20000, file);
+  let mut capacity = 1000;
+  let mut b = Buffer::with_capacity(capacity);
+  let sz = file.read(b.space()).expect("should write");
+  b.fill(sz);
+  println!("write {:#?}", sz);
 
   let length = {
-    let buf = reader.fill_buf()?;
-    println!("data({} bytes):\n{}", buf.len(), (&buf[..min(buf.len(), 128)]).to_hex(16));
-    let res = header(buf);
+    println!("data({} bytes):\n{}", b.available_data(), (&b.data()[..min(b.available_data(), 128)]).to_hex(16));
+    let res = header(b.data());
     //println!("header: {:?}", res);
     if let IResult::Done(remaining, h) = res {
       println!("parsed header: {:#?}", h);
-      buf.offset(remaining)
+      b.data().offset(remaining)
     } else {
       panic!("couldn't parse header");
     }
   };
 
   println!("consumed {} bytes", length);
-  reader.consume(length);
+  b.consume(length);
   // 4 bytes for the size of previous tag
-  reader.consume(4);
+  b.consume(4);
+
   //let buf = reader.fill_buf()?;
   //println!("data after consume and fill_buff ({} bytes):\n{}", buf.len(), (&buf[..min(buf.len(), 128)]).to_hex(16));
 
@@ -63,16 +61,25 @@ fn run(filename: &str) -> std::io::Result<()> {
     let mut tag_count = 0usize;
     let mut consumed = length + 4;
     loop {
-      let (length,tag) = {
-        let buf = reader.fill_buf().expect("should fill buf");
-        println!("[{}] data({} bytes, consumed {}):\n{}", tag_count,
-          buf.len(), consumed, (&buf[..min(buf.len(), 128)]).to_hex(16));
+      if b.available_space() == 0 {
+        println!("growing buffer capacity from {} bytes to {} bytes", capacity, capacity*2);
 
-        if buf.len() == 0 {
+        capacity *= 2;
+        b.grow(capacity);
+      }
+
+      let sz = file.read(b.space()).expect("should write");
+      b.fill(sz);
+
+      let (length,tag) = {
+        println!("[{}] data({} bytes, consumed {}):\n{}", tag_count,
+          b.available_data(), consumed, (&b.data()[..min(b.available_data(), 128)]).to_hex(16));
+
+        if b.available_data() == 0 {
           break;
         }
 
-        match complete_tag(buf) {
+        match complete_tag(b.data()) {
           IResult::Incomplete(needed) => {
             println!("not enough data, needs a refill: {:#?}", needed);
             continue;
@@ -83,12 +90,12 @@ fn run(filename: &str) -> std::io::Result<()> {
           IResult::Done(remaining, tag) => {
             tag_count += 1;
             let t = Tag::new(tag);
-            (buf.offset(remaining), t)
+            (b.data().offset(remaining), t)
           },
         }
       };
 
-      reader.consume(length+4);
+      b.consume(length+4);
       consumed += length+4;
       yield tag;
     }
@@ -107,5 +114,6 @@ fn run(filename: &str) -> std::io::Result<()> {
       }
     }
   }
+
   Ok(())
 }
